e984e7104140f48c061d4f137859358a2d8fe74a
Phumthawan <knzphumthawan@gmail.com>
2019-12-10 15:21:21 +0700

    set server

--- src/index.js
+++ src/index.js
@@ -9,16 +9,79 @@ const port = 3000
 
 const app = express();
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+/**
+ * initail Redis
+ */
+const client = redis.createClient({ host: 'node-js-redis-server' });
+client.on('connect', () => {
+    console.log('Connect to Redis')
+})
 
+/**
+ * set view engine
+ */
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'hbs')
+
+/**
+ * set body parser
+ */
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+/**
+ * user method overide
+ */
 app.use(methodOverride('_method'))
 
+
 app.get('/', (req, res, next) => {
-    res.send('xxx')
+    res.render('searchusers')
+})
+
+app.post('/user/search', (req, res, next) => {
+    const { id } = req.body
+    /**
+     * get object data from Redis
+     */
+    client.hgetall(id, (err, object) => {
+        if (!object) {
+            res.render('searchusers', {
+                error: 'User does not exist.'
+            })
+        } else {
+            object.id = id
+            res.render('details', {
+                user: object
+            })
+        }
+    })
+})
+
+app.get('/user/add', (req, res, next) => {
+    res.render('adduser')
+})
+
+app.post('/user/add', (req, res, next) => {
+    const { id, first_name, last_name, phone, email } = req.body
+    client.hmset(id, [
+        'first_name', first_name,
+        'last_name', last_name,
+        'email', email,
+        'phone', phone
+    ], (err, reply) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log(reply)
+        res.redirect('/')
+    })
+})
+
+app.delete('/user/delete/:id', (req, res, next) => {
+    client.del(req.params.id)
+    res.redirect('/')
 })
 
 app.listen(port, () => {
